a7c9fca3f9d6a5425db1c5886a2b94be7c3414f4
Leigha De La Rosa <cdversiontwo@gmail.com>
2019-11-26 17:00:17 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -84,4 +84,21 @@ def checkout(cart, coupons)
   # * apply_clearance
   #
   # BEFORE it begins the work of calculating the total (or else you might have some irritated customers!
+  consolidated_cart = consolidate_cart(cart)
+  couponed_cart = apply_coupons(cart, coupons)
+  final_cart = 
+  
+  total = 0 
+  i = 0 
+  while i < final_cart.length do 
+    
+    i += 1 
+  end
+  
+  
 end
+
+
+
+
+
